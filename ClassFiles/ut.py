3a0b7a1420d33027e8926eb661efc0c26d588d90
lunz-s <sl767@cam.ac.uk>
2019-04-02 17:28:05 +0200

    UNet signature added

--- ClassFiles/ut.py
+++ ClassFiles/ut.py
@@ -41,9 +41,9 @@ def create_single_folder(folder):
             pass
 
 
-IMAGE_SIZE = [96,96,96]
-FOURIER_SIZE = [96,96,49]
-space = odl.uniform_discr([0, 0, 0], IMAGE_SIZE, IMAGE_SIZE, dtype='float32')
+IMAGE_SIZE = [96, 96, 96]
+FOURIER_SIZE = [96, 96, 49]
+space = odl.uniform_discr([0, 0, 0], [1, 1, 1], IMAGE_SIZE, dtype='float32')
 
 
 class ifftshift_odl(odl.Operator):
@@ -90,5 +90,46 @@ def unify_form(vector):
         return vector
     else:
         raise ValueError('Inputs to the regularizer must have between 3 and 5 dimensions')
+
+# TODO: The adjoint of the Fourier transform is taken to be the inverse, which is not exactly correct when using rfft.
+class DataTerm(odl.solvers.Functional):
+    def __init__(self, domain):
+        super(DataTerm, self).__init__(domain)  # , range=odl.RealNumbers())
+
+    def data_gradient(self, prod_elem):
+
+        image = prod_elem[0]
+        kernel = prod_elem[1]
+        data = prod_elem[2]
+
+        fourier_data = np.fft.fftshift(np.fft.rfftn(image))
+
+        grad = np.multiply(kernel, fourier_data) - data
+
+        return np.fft.ifftshift(np.fft.irfftn(grad))
+
+    def _call(self, prod_elem):
+        return l2(self.data_gradient(prod_elem))  # optimal funtional value
+
+    # For performance OrbitLossGradientOperator should maybe defined outside of orbitLoss?
+    @property
+    def gradient(self):
+        class DataTermGrad(odl.Operator):
+            def __init__(self, domain, outer_instance):
+                super(OrbitLossGradientOperator, self).__init__(domain=domain,
+                                                                range=domain)  # , range=odl.RealNumbers())
+                self.outer_instance = outer_instance
+
+            def _call(self, prod_elem):
+                x = prod_elem[0]  # Ground truth
+                y = prod_elem[1]  # Data
+                vec_x = np.zeros_like(
+                    x)  # Lets set the dervative wrt ground truth-part to zero. It will not propagate back to network params.
+                theta = self.outer_instance.localReg(prod_elem)[0]  # optimal angle
+                vec_y = y - skimage_rot(x, theta, order=3)
+                vec = self.domain.element([vec_x, vec_y])
+                return vec
+
+        return OrbitLossGradientOperator(self.domain, self)
 
 
