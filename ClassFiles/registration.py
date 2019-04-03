2f3dba400bc5c1ce14a2cc08c2ee65acf08f1f90
lunz-s <sl767@cam.ac.uk>
2019-04-03 15:33:17 +0200

    registration added

--- ClassFiles/registration.py
+++ ClassFiles/registration.py
@@ -1,17 +1,21 @@
 from ClassFiles import tensorflow_rotations
 import tensorflow as tf
 import numpy as np
-from ClassFiles.ut import l2
 
 
 class Registrator(object):
+    IMAGE_SIZE = (None, 96, 96, 96, 1)
+    NOISE = 0.01
+    IMAX = 200
+    LEARNING_RATE = 1e-2
 
-    def __init__(self, IMAGE_SIZE):
+    def __init__(self):
         self.sess = tf.InteractiveSession()
 
-        self.image_size = IMAGE_SIZE
-        self.image_feed = tf.placeholder(shape=IMAGE_SIZE, dtype=tf.float32)
-        self.reference_feed = tf.placeholder(shape=IMAGE_SIZE, dtype=tf.float32)
+        self.image_size = self.IMAGE_SIZE
+        self.image_feed = tf.placeholder(shape=self.IMAGE_SIZE, dtype=tf.float32)
+        self.reference_feed = tf.placeholder(shape=self.IMAGE_SIZE, dtype=tf.float32)
+        self.noise = tf.placeholder(shape=(), dtype=tf.float32)
 
         basis_exp = tf.Variable(np.zeros(shape=(3, 3)))
         skew_exp = basis_exp - tf.transpose(basis_exp)
@@ -20,35 +24,50 @@ class Registrator(object):
         self.theta = tf.concat([self.rotation, self.translation], axis=-1)
 
         self.rot_image = tensorflow_rotations.rot3d(self.image_feed, self.theta)
-        self.loss = tf.square(self.rot_image - self.reference_feed)
 
-        self.optimizer = tf.train.AdamOptimizer(learning_rate=1e-2).minimize(self.loss)
+        diff = self.rot_image - self.reference_feed
+        self.data_fit = tf.reduce_sum(tf.square(diff))
+        for i in range(1, 3):
+            self.data_fit += tf.reduce_sum(tf.square(tf.layers.average_pooling3d(diff, 2 ** i, 2 ** i)))
+
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=self.LEARNING_RATE).minimize(self.data_fit)
+
+        with tf.control_dependencies([self.optimizer]):
+            self.assign = tf.group(tf.assign_add(basis_exp, np.pi * self.noise * tf.random_normal(shape=(3, 3))),
+                            tf.assign_add(self.translation, self.noise * tf.random_normal(shape=(3, 1))))
         tf.global_variables_initializer().run()
 
     def register(self, image, reference, printing=True):
         tf.global_variables_initializer().run()
 
-        i = 0
-        end = 10000
-        prev_theta = np.zeros
-        while i <= 10000:
-            _, l, theta = self.sess.run([self.optimizer, self.loss, self.theta],
-                                 feed_dict={self.image_feed: image, self.reference_feed: reference})
-            if i == 0 and printing:
-                print('Initial Registration Loss: ' + str(l))
-            if l2(theta-prev_theta) < 1e-6 and i > 30:
-                i = end
+        for k in range(self.IMAX):
+            _, _, loss = self.sess.run([self.optimizer, self.assign, self.data_fit],
+                                                   feed_dict={self.image_feed: image,
+                                                              self.reference_feed: reference,
+                                                              self.noise: self.NOISE * (k < self.IMAX // 2)})
+            if k % 10 == 0:
+                print(loss)
 
-        result, l, rot, trans = self.sess.run([self.rot_image, self.loss, self.rotation, self.translation],
-                                    feed_dict={self.image_feed: image, self.reference_feed: reference})
+        result, theta, rot, trans, loss = self.sess.run([self.rot_image, self.theta, self.rotation,
+                                                         self.translation, self.data_fit],
+                                                           feed_dict={self.image_feed: image,
+                                                                      self.reference_feed: reference})
 
         if printing:
-            print('Final Registration Loss: ' + str(l))
+            print('Final Registration Loss: ' + str(loss))
             print('Rotation: ')
             print(rot)
             print('Translation: ')
             print(trans)
 
         return result
+
+
+class LocalRegistrator(Registrator):
+
+    NOISE = 0.001
+    IMAX = 50
+    LEARNING_RATE = 1e-2
+
 
 
