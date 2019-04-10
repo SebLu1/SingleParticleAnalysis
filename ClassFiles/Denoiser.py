2a79e1e87a82483c4e1d043e265ac447421e6584
Gustav Zickert <zickert@kth.se>
2019-04-10 15:14:20 +0200

    Update denoising

--- ClassFiles/Denoiser.py
+++ ClassFiles/Denoiser.py
@@ -2,14 +2,16 @@ import os
 import tensorflow as tf
 from ClassFiles.networks import UNet
 import ClassFiles.ut as ut
-from ClassFiles.ut import fftshift_tf
+# from ClassFiles.ut import fftshift_tf
 from ClassFiles.ut import normalize_tf
 
 IMAGE_SIZE = (None, 96, 96, 96, 1)
 FOURIER_SIZE = (None, 96, 96, 49, 1)
 
+
 def data_augmentation_default(gt, adv):
     return gt, adv
+
 
 class Denoiser(object):
 
@@ -29,38 +31,40 @@ class Denoiser(object):
         self.learning_rate = tf.placeholder(dtype=tf.float32)
 
         # Network outputs
-        true_normed, data_normed = data_augmentation(normalize_tf(self.true), normalize_tf(self.data))    
-        self.denoised = self.network.net(data)
+        self.true_normed, self.data_normed = data_augmentation(normalize_tf(self.true),
+                                                     normalize_tf(self.data))
+        self.denoised = self.network.net(self.data_normed)
 
         # Loss
-        self.loss = 0.5 * tf.reduce_sum((self.true - self.denoised) ** 2)
+        self.loss = 0.5 * tf.reduce_sum((self.true_normed - self.denoised) ** 2)
 
         # Optimizer
         self.global_step = tf.Variable(0, name='global_step', trainable=False)
         self.optimizer = tf.train.AdamOptimizer().minimize(self.loss, global_step=self.global_step)
 
         # Logging tools
-        l = []
+        summary_list = []
         with tf.name_scope('Network_Optimization'):
-            l.append(tf.summary.scalar('Data_Difference', self.wasserstein_loss))
-            l.append(tf.summary.scalar('Lipschitz_Regulariser', self.regulariser_was))
-            l.append(tf.summary.scalar('Overall_Net_Loss', self.loss_was))
-            l.append(tf.summary.scalar('Norm_Input_true', tf.norm(self.true)))
-            l.append(tf.summary.scalar('Norm_Input_adv', tf.norm(self.gen)))
-            l.append(tf.summary.scalar('Norm_Gradient', tf.norm(self.gradient)))
+            summary_list.append(tf.summary.scalar('Loss', self.loss))
+ #           l.append(tf.summary.scalar('Data_Difference', self.wasserstein_loss))
+ #           l.append(tf.summary.scalar('Lipschitz_Regulariser', self.regulariser_was))
+ #           l.append(tf.summary.scalar('Overall_Net_Loss', self.loss_was))
+ #           l.append(tf.summary.scalar('Norm_Input_true', tf.norm(self.true)))
+ #           l.append(tf.summary.scalar('Norm_Input_adv', tf.norm(self.gen)))
+ #           l.append(tf.summary.scalar('Norm_Gradient', tf.norm(self.gradient)))
             with tf.name_scope('Maximum_Projection'):
-                l.append(tf.summary.image('Adversarial', tf.reduce_max(gen_normed, axis=3), max_outputs=1))
-                l.append(tf.summary.image('GroundTruth', tf.reduce_max(true_normed, axis=3), max_outputs=1))
-                l.append(tf.summary.image('Gradient_Adv', tf.reduce_max(tf.abs(self.gradient), axis=3), max_outputs=1))
-                l.append(tf.summary.image('Gradient_GT', tf.reduce_max(tf.abs(gradient_track), axis=3), max_outputs=1))
+                summary_list.append(tf.summary.image('Noisy', tf.reduce_max(self.data_normed, axis=3), max_outputs=1))
+                summary_list.append(tf.summary.image('Ground Truth', tf.reduce_max(self.true_normed, axis=3), max_outputs=1))
+#                l.append(tf.summary.image('Gradient_Adv', tf.reduce_max(tf.abs(self.gradient), axis=3), max_outputs=1))
+#                l.append(tf.summary.image('Gradient_GT', tf.reduce_max(tf.abs(gradient_track), axis=3), max_outputs=1))
             slice = int(IMAGE_SIZE[3]/2)
             with tf.name_scope('Slice_Projection'):
-                l.append(tf.summary.image('Adversarial', gen_normed[..., slice, :], max_outputs=1))
-                l.append(tf.summary.image('GroundTruth', true_normed[..., slice, :], max_outputs=1))
-                l.append(tf.summary.image('Gradient_Adv', self.gradient[..., slice, :],  max_outputs=1))
-                l.append(tf.summary.image('Gradient_GT', gradient_track[..., slice, :], max_outputs=1))
+                summary_list.append(tf.summary.image('Noisy', self.data_normed[..., slice, :], max_outputs=1))
+                summary_list.append(tf.summary.image('Ground Truth', self.true_normed[..., slice, :], max_outputs=1))
+ #               l.append(tf.summary.image('Gradient_Adv', self.gradient[..., slice, :],  max_outputs=1))
+ #               l.append(tf.summary.image('Gradient_GT', gradient_track[..., slice, :], max_outputs=1))
 
-            self.merged_network = tf.summary.merge(l)
+            self.merged_network = tf.summary.merge(summary_list)
 
         # set up the logger
         self.writer = tf.summary.FileWriter(self.path + '/Logs/Network_Optimization/')
@@ -71,50 +75,37 @@ class Denoiser(object):
         # load existing saves
         self.load()
 
-#    def evaluate(self, fourierData):
-#        fourierData = ut.unify_form(fourierData)
-#        grad = self.sess.run(self.gradient, feed_dict={self.fourier_data: fourierData})
-#        return ut.adjoint_irfft(grad[0,...,0])
-    
-#    def evaluate_real(self, real_data):
-#        real_data = ut.unify_form(real_data)
-#        grad = self.sess.run(self.gradient, feed_dict={self.gen: real_data})
-#        return (grad[0,...,0])    
+    def evaluate(self, data):
+        return self.denoised.eval(feed_dict={self.data: data})
 
-    # trains the network with the groundTruths and adversarial exemples given. If Flag fourier_data is false,
-    # the adversarial exemples are expected to be in real space
-    def train(self, groundTruth, adversarial, learning_rate, fourier_data =True):
+    def train(self, groundTruth, noisy, learning_rate=1):
         groundTruth = ut.unify_form(groundTruth)
-        adversarial = ut.unify_form(adversarial)
-        if fourier_data:
-            self.sess.run(self.optimizer, feed_dict={self.true: groundTruth, self.fourier_data: adversarial,
-                                                     self.learning_rate: learning_rate})
-        else:
-            self.sess.run(self.optimizer, feed_dict={self.true: groundTruth, self.gen: adversarial,
-                                                     self.learning_rate: learning_rate})
+        noisy = ut.unify_form(noisy)
+        self.sess.run(self.optimizer,
+                      feed_dict={self.true: groundTruth,
+                                 self.data: noisy,
+                                 self.learning_rate: learning_rate})
 
     # Input as in 'train', but writes results to tensorboard instead
-    def test(self, groundTruth, adversarial, fourier_data =True):
+    def test(self, groundTruth, noisy):
         groundTruth = ut.unify_form(groundTruth)
-        adversarial = ut.unify_form(adversarial)
-        if fourier_data:
-            merged, step = self.sess.run([self.merged_network, self.global_step],
-                                         feed_dict={self.true: groundTruth, self.fourier_data: adversarial})
-        else:
-            merged, step = self.sess.run([self.merged_network, self.global_step], 
-                                         feed_dict={self.true: groundTruth, self.gen: adversarial})
+        noisy = ut.unify_form(noisy)
+        merged, step = self.sess.run([self.merged_network, self.global_step],
+                                     feed_dict={self.true: groundTruth,
+                                                self.data: noisy})
         self.writer.add_summary(merged, global_step=step)
-
 
     def save(self):
         saver = tf.train.Saver()
-        saver.save(self.sess, self.path+'/Data/model', global_step=self.global_step)
+        saver.save(self.sess, self.path + '/Data/model',
+                   global_step=self.global_step)
         print('Progress saved')
 
     def load(self):
         saver = tf.train.Saver()
-        if os.listdir(self.path+'/Data/'):
-            saver.restore(self.sess, tf.train.latest_checkpoint(self.path+'/Data/'))
+        if os.listdir(self.path + '/Data/'):
+            saver.restore(self.sess,
+                          tf.train.latest_checkpoint(self.path + '/Data/'))
             print('Save restored')
         else:
             print('No save found')
---
+++ Training_denoiser.py
@@ -0,0 +1,93 @@
+import mrcfile
+import numpy as np
+from ClassFiles.Framework import Denoiser
+from ClassFiles.ut import find
+import random
+
+
+BATCH_SIZE = 1
+LEARNING_RATE = 0.00005
+LOOPS = 5
+STEPS = 1000
+
+GT_PATH = '/local/scratch/public/sl767/MRC_Data/org/'
+SGD_PATH = '/local/scratch/public/sl767/MRC_Data/Data_002_10k/SGD'
+EM_PATH = '/local/scratch/public/sl767/MRC_Data/Data_002_10k/EM'
+
+
+NOISY_PATH = EM_PATH
+
+train_list = []
+for k in range(1, 9):
+    train_list += find('{}*mult002_class001.mrc'.format(k), NOISY_PATH)
+train_amount = len(train_list)
+print('# Training data points: ' + str(train_amount))
+#print(train_list)
+eval_list = find('9*mult002_class001.mrc', NOISY_PATH)
+eval_amount = len(eval_list)
+#print(eval_list)
+print('# Evaluation data points: ' + str(eval_amount))
+
+
+def locate_gt(noisy_path):
+    pdb_id = noisy_path[-30: -26]
+    L = find('*' + pdb_id + '.mrc', GT_PATH)
+    if not len(L) == 1:
+        raise ValueError('non-unique pdb id: ' + str(L))
+    else:
+#        print(pdb_id)
+        return L[0]
+
+
+def get_image(number, training):
+    if training:
+        L = train_list
+    else:
+        L = eval_list
+    adv_path=L[number]
+    with mrcfile.open(adv_path) as mrc:
+        adv = mrc.data
+    with mrcfile.open(locate_gt(adv_path)) as mrc:
+        gt = mrc.data
+    return gt, adv
+
+
+def load_data(training_data=True):
+    if training_data:
+        n = train_amount
+    else:
+        n = eval_amount
+    return get_image(random.randint(0, n), training=training_data)
+
+
+def get_batch(training_data=True):
+    true = np.zeros(shape=(BATCH_SIZE, 96, 96, 96))
+    adv = np.zeros(shape=(BATCH_SIZE, 96, 96, 96))
+    for k in range(BATCH_SIZE):
+        gt, adver = load_data(training_data=training_data)
+        true[k, ...] = gt
+        adv[k, ...] = adver
+    return true, adv
+
+
+saves_path = '/local/scratch/public/sl767/SPA/Saves/Denoiser/Final_EM_trained'
+denoiser = Denoiser(saves_path)
+
+
+def evaluate():
+    gt, adv = get_batch(training_data=True)
+    denoiser.test(groundTruth=gt, adversarial=adv)
+
+
+def train(steps):
+    for k in range(steps):
+        gt, adv = get_batch()
+        denoiser.train(groundTruth=gt, adversarial=adv,
+                       learning_rate=LEARNING_RATE)
+        if k % 5 == 0:
+            evaluate()
+    denoiser.save()
+
+
+for k in range(LOOPS):
+    train(STEPS)
