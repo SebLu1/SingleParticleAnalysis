8f0840c5d81cd61ffbe5b2b59cd0a0a0ec8ae2fb
Gustav Zickert <zickert@radon.math.kth.se>
2019-07-16 13:34:04 +0200

    Improve run relion stuff

---
+++ LowPassIni/Training_AR.py
@@ -0,0 +1,89 @@
+import platform
+PLATFORM_NODE = platform.node()
+import sys
+if PLATFORM_NODE == 'motel':
+    sys.path.insert(0, '/home/sl767/PythonCode/SingleParticleAnalysis')
+if PLATFORM_NODE == 'radon':
+    sys.path.insert(0, '/home/zickert/SingleParticleAnalysis')
+from ClassFiles.AdversarialRegularizer import AdversarialRegulariser
+from ClassFiles.DataAugmentation import (interpolation, phase_augmentation,
+                                        rotation_translation, positivity)
+from ClassFiles.DataGeneration import get_batch, get_dict
+
+SAVES_PATH = '/mnt/datahd/zickert/SPA/Saves/SimDataPaper/'
+SAVES_PATH += 'Adversarial_Regulariser/Cutoff_20/Roto-Translation_Augmentation'
+
+PAPER_DATA_PATH = '/local/scratch/public/sl767/MRC_Data/Data/SimDataPaper/'
+LOG_FILE_PATH = SAVES_PATH + '/train_eval_data_logfile.txt'
+
+
+BATCH_SIZE = 1
+LEARNING_RATE = 0.00005
+LOOPS = 2
+STEPS = 5000
+
+# Parameter choices. Heuristic in the BWGAN paper:
+# Choose GAMMA as average dual norm of clean image
+# LMB should be bigger than product of norm times dual norm.
+
+# For s=0.0, this implies GAMMA =1.0
+# For s=1.0, have GAMMA = 10.0 as realisitc value
+S = 1.0
+LMB = 10.0
+GAMMA = 5.0
+CUTOFF = 20.0
+
+TRAIN_NOISE_LEVELS = ['01', '02']
+TRAIN_METHODS = ['EM', 'SGD']
+EVAL_NOISE_LEVELS = ['01', '016']
+EVAL_METHODS = ['EM', 'SGD']
+
+TRAIN_DICT = get_dict(TRAIN_NOISE_LEVELS, TRAIN_METHODS, eval_data=False,
+                      data_path=PAPER_DATA_PATH)
+EVAL_DICT = get_dict(EVAL_NOISE_LEVELS, EVAL_METHODS, eval_data=True,
+                     data_path=PAPER_DATA_PATH)
+
+
+def data_augmentation(gt, adv):
+    _, adv1 = interpolation(gt, adv)
+    _, adv2 = phase_augmentation(gt, adv1)
+    _, adv3 = positivity(gt, adv2)
+    new_gt, new_adv = rotation_translation(gt, adv3)
+    return new_gt, new_adv
+
+
+regularizer = AdversarialRegulariser(SAVES_PATH, data_augmentation,
+                                     s=S, cutoff=20.0, lmb=LMB, gamma=GAMMA)
+
+log_file = open(LOG_FILE_PATH, "w")
+log_file.write('Train data:\n' + str(TRAIN_DICT) + '\n')
+log_file.write('Eval data:\n' + str(EVAL_DICT) + '\n')
+log_file.close()
+
+
+
+def evaluate():
+    gt, adv = get_batch(batch_size=BATCH_SIZE, noise_levels=EVAL_NOISE_LEVELS,
+                        methods=EVAL_METHODS, data_dict=EVAL_DICT)
+    regularizer.test(groundTruth=gt, adversarial=adv)
+
+
+def train(steps):
+    for k in range(steps):
+        gt, adv = get_batch(batch_size=BATCH_SIZE,
+                            noise_levels=TRAIN_NOISE_LEVELS,
+                            methods=TRAIN_METHODS, data_dict=TRAIN_DICT)
+        regularizer.train(groundTruth=gt, adversarial=adv,
+                          learning_rate=LEARNING_RATE)
+        if k % 50 == 0:
+            evaluate()
+    regularizer.save()
+
+
+for k in range(LOOPS):
+    train(STEPS)
+
+LEARNING_RATE = 0.00002
+
+for k in range(LOOPS):
+    train(STEPS)
--- LowPassIni/run_relion_stuff.py
+++ LowPassIni/run_relion_stuff.py
@@ -23,19 +23,23 @@ parser.add_argument('--pdb_id', help='PDB ID (if none, give 0)',
                     required=True)
 parser.add_argument('--noise', help='Noise levels',
                     required=True)
-parser.add_argument('--ext', help='Which exernal reco? (0, def, def_pos, AR, AR_pos, RED)',
+parser.add_argument('--ext', help='Which exernal reco? (0, def, def_pos, AR, AR_pos, RED, naive_den)',
                     required=True)
 parser.add_argument('--mask', help='Use mask and solvent_correct_fsc?',
+                    required=True)
+parser.add_argument('--reg_par', help='AR reg. parameter')
+#                    ,required=True)
+parser.add_argument('--num_mpi', help='MPI nodes',
                     required=True)
 args = vars(parser.parse_args())
 
 if platform.node() == 'radon':
-    BASE_PATH = '/home/zickert/'
+    CODE_PATH = '/home/zickert/'
 
 GPU_ids = args['gpu']
 print(GPU_ids)
 
-NUM_MPI = 3   # At least 3 if --split_random_halves is useds
+NUM_MPI = int(args['num_mpi'])   # At least 3 if --split_random_halves is useds
 
 mk_dirs = True
 create_projs = int(args['projs'])
@@ -46,24 +50,41 @@ START_MOL = int(args['pdb_start_idx'])
 END_MOL = int(args['pdb_end_idx'])
 MASK = int(args['mask'])
 
+
 noise_level = args['noise'].split(' ')
 
 EXT_RECO_MODE = args['ext']
 if EXT_RECO_MODE == '0':
     METHOD = 'EM'
 else:
-    METHOD = EXT_RECO_MODE
+    if EXT_RECO_MODE == 'AR' or EXT_RECO_MODE == 'AR_pos':
+        if 'reg_par' in args:
+            REG_PAR = float(args['reg_par'])
+        else:
+            raise Exception
+        METHOD = EXT_RECO_MODE + '_' + args['reg_par']
+    else:                    
+        METHOD = EXT_RECO_MODE
 if EXT_RECO_MODE == 'def':
     os.environ['RELION_EXTERNAL_RECONSTRUCT_EXECUTABLE'] = 'relion_external_reconstruct' # Default
     print('EXT_RECO: ' + os.environ['RELION_EXTERNAL_RECONSTRUCT_EXECUTABLE'])
 if EXT_RECO_MODE == 'def_pos':
-    os.environ['RELION_EXTERNAL_RECONSTRUCT_EXECUTABLE'] = BASE_PATH + 'SingleParticleAnalysis/LowPassIni/ext_reconstruct_CLASSICAL_positivity.py' # 
+    os.environ['RELION_EXTERNAL_RECONSTRUCT_EXECUTABLE'] = CODE_PATH + 'SingleParticleAnalysis/LowPassIni/ext_reconstruct_CLASSICAL_positivity.py' 
     print('EXT_RECO: ' + os.environ['RELION_EXTERNAL_RECONSTRUCT_EXECUTABLE'])    
 elif EXT_RECO_MODE == 'AR':
-    raise NotImplementedError
+    os.environ['RELION_EXTERNAL_RECONSTRUCT_EXECUTABLE'] = CODE_PATH + 'SingleParticleAnalysis/LowPassIni/ext_reconstruct_AR.py'
+    print('EXT_RECO: ' + os.environ['RELION_EXTERNAL_RECONSTRUCT_EXECUTABLE'])
+    os.environ['RELION_EXTERNAL_RECONSTRUCT_REGULARIZATION'] = REG_PAR
+elif EXT_RECO_MODE == 'AR_pos':
+    os.environ['RELION_EXTERNAL_RECONSTRUCT_EXECUTABLE'] = CODE_PATH + 'SingleParticleAnalysis/LowPassIni/ext_reconstruct_AR_positive.py'
+    print('EXT_RECO: ' + os.environ['RELION_EXTERNAL_RECONSTRUCT_EXECUTABLE'])
+    os.environ['RELION_EXTERNAL_RECONSTRUCT_REGULARIZATION'] = REG_PAR
 elif EXT_RECO_MODE == 'RED':
     raise NotImplementedError
-
+elif EXT_RECO_MODE == 'naive_den':
+    raise NotImplementedError
+                
+                    
 if platform.node() == 'radon':
     BASE_PATH = '/mnt/datahd/zickert/MRC_Data'
 MPI_MODE = 'mpirun'
@@ -73,13 +94,22 @@ def runCommand(cmd_string, shell=False):
     sp.call(cmd_string.split(' '), shell=shell)
 
 
-def find_PDB_ID(pattern, path):
-    result = []
+def find_PDB_ID(pattern, path, sort=True):
+    result_tmp = []
     for root, dirs, files in os.walk(path):
         for name in files:
             if fnmatch.fnmatch(name, pattern):
-                result.append((os.path.join(root, name).replace("\\",
+                result_tmp.append((os.path.join(root, name).replace("\\",
                                "/"))[-8:-4])
+    if sort:
+        result_tmp.sort()
+        result = []
+        for j in range(len(result_tmp)):
+            if j == 0 or result_tmp[j][:3] != result_tmp[j-1][:3]:
+                result.append(result_tmp[j])
+        result.sort()   
+    else:
+        result = result_tmp
     return result
 
 
@@ -107,7 +137,7 @@ if args['pdb_id'] is not '0':
     
 print('PDB ids: ', PDB_ID)
 print('Eval data: ', EVAL_DATA)
-#input("Looks alright?")
+input("Looks alright?")
 
 if mk_dirs:
     for p in PDB_ID:
@@ -127,64 +157,71 @@ if create_projs:
     # Scale phantoms
     for p in PDB_ID:
         for n in noise_level:
-            mult_cmd = 'relion_image_handler --i {OrgP}/{p1}/{p}.mrc --multiply_constant 0.{n}'
-            mult_cmd += ' --o {OP}/mult_maps/{p}/{p}_mult0{n}.mrc'
-            mult_cmd = mult_cmd.format(OrgP=ORG_PATH, OP=out_path, p=p, p1=p[0], n=n)
-            runCommand(mult_cmd.format(N=n))
+            if not os.path.isfile('{OP}/mult_maps/{p}/{p}_mult0{n}.mrc'.format(OP=out_path, p=p, n=n).format(N=n)):
+                mult_cmd = 'relion_image_handler --i {OrgP}/{p1}/{p}.mrc --multiply_constant 0.{n}'
+                mult_cmd += ' --o {OP}/mult_maps/{p}/{p}_mult0{n}.mrc'
+                mult_cmd = mult_cmd.format(OrgP=ORG_PATH, OP=out_path, p=p, p1=p[0], n=n)
+                runCommand(mult_cmd.format(N=n))
     # Create noisy projections
     for p in PDB_ID:
         for n in noise_level:
-            proj_cmd = 'relion_project --i {OP}/mult_maps/{p}/{p}_mult0{n}.mrc'
-            proj_cmd += ' --o {OP}/projs/{p}/{p}_mult0{n} --nr_uniform 10000'
-            proj_cmd += ' --sigma_offset 2 --add_noise --white_noise 1' 
-            proj_cmd = proj_cmd.format(OP=out_path, p=p, n=n)
-            runCommand(proj_cmd.format(N=n))
+            if not os.path.isfile('{OP}/projs/{p}/{p}_mult0{n}.mrcs'.format(OP=out_path, p=p, n=n).format(N=n)):
+                proj_cmd = 'relion_project --i {OP}/mult_maps/{p}/{p}_mult0{n}.mrc'
+                proj_cmd += ' --o {OP}/projs/{p}/{p}_mult0{n} --nr_uniform 10000'
+                proj_cmd += ' --sigma_offset 2 --add_noise --white_noise 1' 
+                proj_cmd = proj_cmd.format(OP=out_path, p=p, n=n)
+                runCommand(proj_cmd.format(N=n))
 
 if run_EM:
     for p in PDB_ID:
         for n in noise_level:
             if MASK:
-                mask_cmd = 'relion_mask_create --i {OrgP}/{p1}/{p}.mrc'
-                mask_cmd += ' --o {OP}/masks/{p}/mask.mrc'
-                mask_cmd += ' --ini_threshold 0.175 --extend_inimask 0 --width_soft_edge 5 --lowpass 30 --angpix 1.5'
-                mask_cmd = mask_cmd.format(OrgP=ORG_PATH, OP=out_path, p=p, p1=p[0], n=n)
-                print(mask_cmd)
-                runCommand(mask_cmd.format(N=n))
-            if MPI_MODE == 'mpirun':
-                refine_cmd = 'mpirun -n {NUM_MPI} relion_refine_mpi'
-            elif MPI_MODE == 'srun':
-                refine_cmd = 'srun --mpi=pmi2 relion_refine_mpi'
-            elif MPI_MODE == 'mpirun-hpc':
-                refine_cmd = 'mpirun relion_refine_mpi'
+                    check_path = '{OP}/{meth}_masked/{p}/{p}_mult0{n}.mrc'.format(OP=out_path, p=p, n=n, meth=METHOD).format(N=n)
             else:
-                raise Exception 
-            if MASK:
-                refine_cmd += ' --solvent_mask {OP}/masks/{p}/mask.mrc --solvent_correct_fsc'
-                refine_cmd += ' --o {OP}/{meth}_masked/{p}/{p}_mult0{n}'
-            else:
-                refine_cmd += ' --o {OP}/{meth}/{p}/{p}_mult0{n}'
-            refine_cmd += ' --auto_refine --split_random_halves'
-            refine_cmd += ' --i {OP}/projs/{p}/{p}_mult0{n}.star'
-            refine_cmd += ' --ref {OP}/mult_maps/{p}/{p}_mult0{n}.mrc' 
-            refine_cmd += ' --ini_high 30'
-            refine_cmd += ' --pad 1'
-            refine_cmd += ' --particle_diameter 150 --flatten_solvent --zero_mask --oversampling 1'
-            refine_cmd += ' --healpix_order 2 --offset_range 5'
-            refine_cmd += ' --auto_local_healpix_order 4'
-            refine_cmd += ' --offset_step 2 --sym C1'
-            refine_cmd += ' --low_resol_join_halves 40'
-            refine_cmd += ' --norm --scale'
-            refine_cmd += ' --gpu {GPU_ids}'
-            if EXT_RECO_MODE is not '0':
-                refine_cmd += ' --external_reconstruct'
-#--maximum_angular_sampling 1.8'
-            refine_cmd += ' --j 6' # Number of threads to run in parallel (only useful on multi-core machines)
-            refine_cmd += ' --pool 30' # Number of images to pool for each thread task
-            refine_cmd += ' --dont_combine_weights_via_disc'  # Send the large arrays of summed weights through the MPI network,
-                                                              # instead of writing large files to disc
-#            refine_cmd += ' --auto_iter_max 1'    
-#            refine_cmd += ' --iter 30'
-#            refine_cmd += ' --preread_images' #  Use this to let the master process read all particles into memory.
-                                               #  Be careful you have enough RAM for large data sets!
-            refine_cmd = refine_cmd.format(OP=out_path, p=p, n=n, GPU_ids=GPU_ids, NUM_MPI=NUM_MPI, meth=METHOD)
-            runCommand(refine_cmd.format(N=n)) 
+                    check_path = '{OP}/{meth}/{p}/{p}_mult0{n}.mrc'.format(OP=out_path, p=p, n=n, meth=METHOD).format(N=n)
+            if not os.path.isfile(check_path):
+                if MASK:
+                    mask_cmd = 'relion_mask_create --i {OrgP}/{p1}/{p}.mrc'
+                    mask_cmd += ' --o {OP}/masks/{p}/mask.mrc'
+                    mask_cmd += ' --ini_threshold 0.175 --extend_inimask 0 --width_soft_edge 5 --lowpass 30 --angpix 1.5'
+                    mask_cmd = mask_cmd.format(OrgP=ORG_PATH, OP=out_path, p=p, p1=p[0], n=n)
+                    print(mask_cmd)
+                    runCommand(mask_cmd.format(N=n))
+                if MPI_MODE == 'mpirun':
+                    refine_cmd = 'mpirun -n {NUM_MPI} relion_refine_mpi'
+                elif MPI_MODE == 'srun':
+                    refine_cmd = 'srun --mpi=pmi2 relion_refine_mpi'
+                elif MPI_MODE == 'mpirun-hpc':
+                    refine_cmd = 'mpirun relion_refine_mpi'
+                else:
+                    raise Exception 
+                if MASK:
+                    refine_cmd += ' --solvent_mask {OP}/masks/{p}/mask.mrc --solvent_correct_fsc'
+                    refine_cmd += ' --o {OP}/{meth}_masked/{p}/{p}_mult0{n}'
+                else:
+                    refine_cmd += ' --o {OP}/{meth}/{p}/{p}_mult0{n}'
+                refine_cmd += ' --auto_refine --split_random_halves'
+                refine_cmd += ' --i {OP}/projs/{p}/{p}_mult0{n}.star'
+                refine_cmd += ' --ref {OP}/mult_maps/{p}/{p}_mult0{n}.mrc' 
+                refine_cmd += ' --ini_high 30'
+                refine_cmd += ' --pad 1'
+                refine_cmd += ' --particle_diameter 150 --flatten_solvent --zero_mask --oversampling 1'
+                refine_cmd += ' --healpix_order 2 --offset_range 5'
+                refine_cmd += ' --auto_local_healpix_order 4'
+                refine_cmd += ' --offset_step 2 --sym C1'
+                refine_cmd += ' --low_resol_join_halves 40'
+                refine_cmd += ' --norm --scale'
+                refine_cmd += ' --gpu {GPU_ids}'
+                if EXT_RECO_MODE is not '0':
+                    refine_cmd += ' --external_reconstruct'
+    #--maximum_angular_sampling 1.8'
+                refine_cmd += ' --j 6' # Number of threads to run in parallel (only useful on multi-core machines)
+                refine_cmd += ' --pool 30' # Number of images to pool for each thread task
+                refine_cmd += ' --dont_combine_weights_via_disc'  # Send the large arrays of summed weights through the MPI network,
+                                                                  # instead of writing large files to disc
+    #            refine_cmd += ' --auto_iter_max 1'    
+    #            refine_cmd += ' --iter 30'
+    #            refine_cmd += ' --preread_images' #  Use this to let the master process read all particles into memory.
+                                                   #  Be careful you have enough RAM for large data sets!
+                refine_cmd = refine_cmd.format(OP=out_path, p=p, n=n, GPU_ids=GPU_ids, NUM_MPI=NUM_MPI, meth=METHOD)
+                runCommand(refine_cmd.format(N=n)) 
